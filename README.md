3c08fde5c3137e718a923d2e30091a99b59c31d1
Pete Moore <pmoore@mozilla.com>
2017-09-26 09:49:25 +0200

    Now require go 1.8 since that is indirectly required by taskcluster-client-go

--- README.md
+++ README.md
@@ -38,7 +38,7 @@ proxy server for linux/amd64 and deploy the server to a docker image.
 
 ## Download via `go get`
 
-__You will need at least go 1.7__. A newer version of go 1.x is also ok.
+__You will need at least go 1.8__. A newer version of go 1.x is also ok.
 
 Set up your [GOPATH](https://golang.org/doc/code.html)
 
--- build.sh
+++ build.sh
@@ -22,12 +22,29 @@ fi
 # step into directory of script
 cd "$(dirname "${0}")"
 
+GO_VERSION="$(go version 2>/dev/null | cut -f3 -d' ')"
+GO_MAJ="$(echo "${GO_VERSION}" | cut -f1 -d'.')"
+GO_MIN="$(echo "${GO_VERSION}" | cut -f2 -d'.')"
+if [ -z "${GO_VERSION}" ]; then
+  echo "Have you installed go? I get no result from \`go version\` command." >&2
+  exit 64
+elif [ "${GO_MAJ}" != "go1" ] || [ "${GO_MIN}" -lt 8 ]; then
+  echo "Go version go1.x needed, where x >= 8, but the version I found is: '${GO_VERSION}'" >&2
+  echo "I found it here:" >&2
+  which go >&2
+  echo "The complete output of \`go version\` command is:" >&2
+  go version >&2
+  exit 65
+else
+  echo "Go version ok! (${GO_VERSION})"
+fi
+
 uid="$(date +%s)"
 
 # Output folder
 mkdir -p target
 
-echo "Generating ca certs using latest ubuntu version..."
+echo "Fetching ca certs from latest ubuntu version..."
 docker build --pull -t "${uid}" -f cacerts.docker .
 docker run --name "${uid}" "${uid}"
 docker cp "${uid}:/etc/ssl/certs/ca-certificates.crt" target
