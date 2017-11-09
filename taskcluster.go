b9c9204b5d52633f2afaab629a38b001e62c281c
Pete Moore <pmoore@mozilla.com>
2017-11-09 16:43:13 +0100

    Fixed to work with new taskcluster-client-go

--- taskcluster.go
+++ taskcluster.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	tcclient "github.com/taskcluster/taskcluster-client-go"
 	"github.com/taskcluster/taskcluster-client-go/queue"
 )
 
 func getTaskScopes(taskId string) ([]string, error) {
 	// We do not need auth for this operation
-	q := queue.New(
-		&tcclient.Credentials{},
-	)
-	q.Authenticate = false
+	q := queue.NewNoAuth()
 
 	task, err := q.Task(taskId)
 	if err != nil {
