2923c414f03c7fe148348d18ad81f5a13bea7c31
Pete Moore <pmoore@mozilla.com>
2019-04-23 16:45:34 +0200

    Bug 1546384 - Hard code rootURL as https://taskcluster.net

--- taskcluster.go
+++ taskcluster.go
@@ -4,7 +4,12 @@ import "github.com/taskcluster/taskcluster-client-go/tcqueue"
 
 func getTaskScopes(taskId string) ([]string, error) {
 	// We do not need auth for this operation
-	q := tcqueue.New(nil)
+	//
+	// NOTE: currently, the only supported rootURL is https://taskcluster.net !!
+	//
+	// If this needs to change, remove this hardcoded reference, and make it a
+	// (required) command line parameter to be passed to the proxy on start up.
+	q := tcqueue.New(nil, "https://taskcluster.net")
 	task, err := q.Task(taskId)
 	if err != nil {
 		return nil, err
